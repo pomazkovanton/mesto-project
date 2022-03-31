9e5987319a23b61e35835e7b2d53d924fca60d7b
Антон Помазков <antonpomazkov@yandex.ru>
2022-03-31 10:34:29 +0800

    refactor: changes the naming and script for the modal window

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-const modal = document.querySelector(".modal-edit");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".modal-edit__btn-close");
+const popup = document.querySelector(".popup");
+const closeBtn = document.querySelector(".popup__btn-close");
 const editBtn = document.querySelector(".profile__btn-edit");
 
 closeBtn.addEventListener("click", function () {
-  modal.classList.add("modal-edit_hide");
-  overlay.classList.add("overlay_hide");
+  popup.classList.add("popup_hide");
 });
 
 editBtn.addEventListener("click", function () {
-  modal.classList.remove("modal-edit_hide");
-  overlay.classList.remove("overlay_hide");
+  popup.classList.remove("popup_hide");
 });
