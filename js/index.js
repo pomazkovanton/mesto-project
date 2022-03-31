8f130c1d8958ca35a92241cbe80e379923a69919
Антон Помазков <antonpomazkov@yandex.ru>
2022-03-31 11:24:59 +0800

    feat: adds a script for likes of posts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const popup = document.querySelector(".popup");
 const closeBtn = document.querySelector(".popup__btn-close");
 const editBtn = document.querySelector(".profile__btn-edit");
+const likeBtns = document.querySelectorAll(".gallery__btn-like");
 
 closeBtn.addEventListener("click", function () {
   popup.classList.add("popup_hide");
@@ -8,4 +9,10 @@ closeBtn.addEventListener("click", function () {
 
 editBtn.addEventListener("click", function () {
   popup.classList.remove("popup_hide");
+});
+
+likeBtns.forEach(function (likeBtn) {
+  likeBtn.addEventListener("click", function () {
+    likeBtn.classList.toggle("gallery__btn-like_active");
+  });
 });
