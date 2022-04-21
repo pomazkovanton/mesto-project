bdf7563048a779cb1058ba4031595aaf14fa91a5
Anton Pomazkov <antonpomazkov@yandex.ru>
2022-04-21 09:41:06 +0800

    feat: add initial data for cards

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
-const btnEdit = document.querySelector('.profile__btn-edit');
+const btnEdit = document.querySelector(".profile__btn-edit");
 
-const popupEdit = document.querySelector('.popup_type_edit');
+const popupEdit = document.querySelector(".popup_type_edit");
 
-const closeBtnEdit = popupEdit.querySelector('.popup__btn-close');
+const closeBtnEdit = popupEdit.querySelector(".popup__btn-close");
 
-let nameUser = document.querySelector('.profile__name');
-let positionUser = document.querySelector('.profile__position');
+let nameUser = document.querySelector(".profile__name");
+let positionUser = document.querySelector(".profile__position");
 
-let inputName = popupEdit.querySelector('.popup__form-input_type_name')
-let inputPosition = popupEdit.querySelector('.popup__form-input_type_position')
+let inputName = popupEdit.querySelector(".popup__form-input_type_name");
+let inputPosition = popupEdit.querySelector(".popup__form-input_type_position");
 
-const formEdit = popupEdit.querySelector('.popup__form');
+const formEdit = popupEdit.querySelector(".popup__form");
 
-function closePopup (popup) {
-  popup.classList.add('popup_hide');
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+function closePopup(popup) {
+  popup.classList.add("popup_hide");
 }
 
-function openPopupEdit () {
+function openPopupEdit() {
   inputName.value = nameUser.textContent;
   inputPosition.value = positionUser.textContent;
-  popupEdit.classList.remove('popup_hide');
+  popupEdit.classList.remove("popup_hide");
 }
 
-function formEditSubmitHandler (evt) {
+function formEditSubmitHandler(evt) {
   evt.preventDefault();
 
   nameUser.textContent = inputName.value;
@@ -32,8 +59,6 @@ function formEditSubmitHandler (evt) {
 }
 
 //Обработка событий для модального окна редактирования профиля
-btnEdit.addEventListener('click', openPopupEdit)
-formEdit.addEventListener('submit', formEditSubmitHandler);
-closeBtnEdit.addEventListener('click', () => closePopup(popupEdit));
-
-
+btnEdit.addEventListener("click", openPopupEdit);
+formEdit.addEventListener("submit", formEditSubmitHandler);
+closeBtnEdit.addEventListener("click", () => closePopup(popupEdit));
