a4cf7e8932a2c1f05c6b8794cb0a3c9ea2d920df
pomazkovanton <vitrumbeta@gmail.com>
2022-07-21 11:27:04 +0800

    feat: add a class for a popup with an image

---
+++ src/components/PopupWithImage.js
@@ -0,0 +1,20 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(selector, imageSelector, captionSelector) {
+    super(selector);
+    this._image = this._popup.querySelector(imageSelector);
+    this._caption = this._popup.querySelector(captionSelector);
+  }
+
+  open(name, url) {
+    this._image.src = url;
+    this._image.alt = name;
+    this._caption.textContent = name;
+
+    super.open();
+  }
+}
+
+
+
