575a385462452063b7d654ad8b929ab9e154f7b3
Adelina Latypova <lat1807@mail.ru>
2022-07-26 15:01:05 +0300

    Make a project structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,10 @@
-import '../css/pages/index.css';
-import {enableValidation, disablingButton, handleClearForm} from './validate';
-import { createCard  } from './card';
-import { openPopup, closePopup, handleOverlayClick } from './popup';
-import { selectorsForm } from './data';
-import { getUser, getCards, postCards, updateUser, updateAvatar } from './api';
+import { enableValidation, disablingButton, handleClearForm } from "./validate";
+import { createCard } from "./card";
+import { openPopup, closePopup, handleOverlayClick } from "./popup";
+import { selectorsForm } from "./data";
+import { getUser, getCards, postCards, updateUser, updateAvatar } from "./api";
 
-let myID = '';
+let myID = "";
 
 // Переменные профиля пользователя
 const btnEdit = document.querySelector(".profile__btn-edit");
@@ -17,15 +16,23 @@ const avatarUser = document.querySelector(".profile__avatar");
 // Переменные модального окна изменения аватара пользователя
 const popupEditAvatar = document.querySelector(".popup_type_avatar");
 const formEditAvatar = popupEditAvatar.querySelector(".popup__form");
-const btnSubmitEditAvatar = formEditAvatar.querySelector(selectorsForm.submitButtonSelector);
-const inputAvatarSrc = formEditAvatar.querySelector(".popup__form-input_type_avatar");
+const btnSubmitEditAvatar = formEditAvatar.querySelector(
+  selectorsForm.submitButtonSelector
+);
+const inputAvatarSrc = formEditAvatar.querySelector(
+  ".popup__form-input_type_avatar"
+);
 
 // Переменные модального окна изменения профиля
 const popupEdit = document.querySelector(".popup_type_edit");
 const formEdit = popupEdit.querySelector(".popup__form");
-const btnSubmitEdit = formEdit.querySelector(selectorsForm.submitButtonSelector);
+const btnSubmitEdit = formEdit.querySelector(
+  selectorsForm.submitButtonSelector
+);
 const inputName = popupEdit.querySelector(".popup__form-input_type_name");
-const inputPosition = popupEdit.querySelector(".popup__form-input_type_position");
+const inputPosition = popupEdit.querySelector(
+  ".popup__form-input_type_position"
+);
 
 // Кнопка открытия модального окна добавления новой карточки
 const btnAdd = document.querySelector(".profile__btn-add");
@@ -38,16 +45,20 @@ const inputPlace = popupAdd.querySelector(".popup__form-input_type_place");
 const inputImg = popupAdd.querySelector(".popup__form-input_type_img");
 
 // Переменные галереи
-const gallery = document.querySelector('.gallery__list');
+const gallery = document.querySelector(".gallery__list");
 
 // Переменные модальных окон
 const closeButtons = document.querySelectorAll(".popup__btn-close");
 const popups = document.querySelectorAll(".popup");
 
 const renderLoading = (isLoading = false, typeBtnSubmit) => {
-  const button = document.querySelector(`.popup__form-btn_type_${typeBtnSubmit}`);
-  isLoading ? button.textContent = 'Сохранение...' : button.textContent = 'Сохранить';
-}
+  const button = document.querySelector(
+    `.popup__form-btn_type_${typeBtnSubmit}`
+  );
+  isLoading
+    ? (button.textContent = "Сохранение...")
+    : (button.textContent = "Сохранить");
+};
 
 //Функция открытия окна редактирования профиля
 const openPopupEdit = () => {
@@ -56,73 +67,73 @@ const openPopupEdit = () => {
   inputPosition.value = positionUser.textContent;
   disablingButton(btnSubmitEdit, selectorsForm.inactiveButtonSelector);
   openPopup(popupEdit);
-}
+};
 
 //Функция открытия окна изменения аватара
 const openPopupEditAvatar = () => {
   handleClearForm(popupEditAvatar, selectorsForm);
   disablingButton(btnSubmitEditAvatar, selectorsForm.inactiveButtonSelector);
   openPopup(popupEditAvatar);
-}
+};
 
 //Функция открытия окна добавления карточки
 const openPopupAdd = () => {
   disablingButton(btnSubmitAdd, selectorsForm.inactiveButtonSelector);
   handleClearForm(popupAdd, selectorsForm);
   openPopup(popupAdd);
-}
+};
 
 //Функция добавления новой карточки
 const addCard = (namePlace, linkImg, likes, cardID, userID) => {
   const card = createCard(namePlace, linkImg, likes, cardID, userID, myID);
   gallery.prepend(card);
-}
+};
 
 //Функция отрисовки карточек
 const renderCards = (cards) => {
-  cards.forEach(({name, link, likes, owner, _id}) => {
-    addCard(name, link, likes, _id, owner._id)
+  cards.forEach(({ name, link, likes, owner, _id }) => {
+    addCard(name, link, likes, _id, owner._id);
   });
-}
+};
 
 //Функция для обработки отправки формы изменения профиля
 const handleProfileFormSubmit = async (evt) => {
   evt.preventDefault();
-  renderLoading(true, 'edit');
+  renderLoading(true, "edit");
 
   const user = await updateUser(inputName.value, inputPosition.value);
-  const {name, about} = user.data;
+  const { name, about } = user.data;
   nameUser.textContent = name;
   positionUser.textContent = about;
 
   closePopup(popupEdit);
-  renderLoading(false, 'edit');
-}
+  renderLoading(false, "edit");
+};
 
 //Функция для обработки отправки формы добавления новой карточки
 const handleCardFormSubmit = async (evt) => {
   evt.preventDefault();
 
-  renderLoading(true, 'add');
+  renderLoading(true, "add");
   const card = await postCards(inputPlace.value, inputImg.value);
-  const {name, link, likes, _id, owner} = card.data;
+  const { name, link, likes, _id, owner } = card.data;
 
   addCard(name, link, likes, _id, owner._id);
   closePopup(popupAdd);
 
-  renderLoading(false, 'add');
-}
+  renderLoading(false, "add");
+};
 
 //Функция для обработки отправки формы изменения аватара пользователя
 const handleAvatarFormSubmit = async (evt) => {
   evt.preventDefault();
 
-  renderLoading(true, 'avatar');
-  const {data} = await updateAvatar(inputAvatarSrc.value);
+  renderLoading(true, "avatar");
+  const { data } = await updateAvatar(inputAvatarSrc.value);
   avatarUser.src = data.avatar;
   closePopup(popupEditAvatar);
-  renderLoading(false, 'avatar')
-}
+  renderLoading(false, "avatar");
+};
 
 //Обработка событий для модального окна изменения аватара пользователя
 btnEditAvatar.addEventListener("click", openPopupEditAvatar);
@@ -137,9 +148,9 @@ btnAdd.addEventListener("click", openPopupAdd);
 formAdd.addEventListener("submit", handleCardFormSubmit);
 
 // Обработка событий закрытия модальных окон
-popups.forEach(popup => {
+popups.forEach((popup) => {
   handleOverlayClick(popup);
-})
+});
 
 //Обработка нажатия по кнопки закрытия модального окна
 closeButtons.forEach((button) => {
@@ -153,7 +164,7 @@ enableValidation(selectorsForm);
 //Получение карточек и данных о пользователе с сервера
 const getData = async () => {
   const serverData = await Promise.all([getUser(), getCards()]);
-  const {_id, name, about, avatar} = serverData[0].data;
+  const { _id, name, about, avatar } = serverData[0].data;
   const cards = serverData[1].data;
 
   myID = _id;
@@ -161,7 +172,7 @@ const getData = async () => {
   positionUser.textContent = about;
   avatarUser.src = avatar;
 
-  renderCards(cards.reverse())
-}
+  renderCards(cards.reverse());
+};
 
 getData();
