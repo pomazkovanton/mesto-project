200c5488da12167db755c1ba1033194f59ca8eee
pomazkovanton <vitrumbeta@gmail.com>
2022-07-26 20:50:38 +0800

    feat: add class Section

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -2,13 +2,21 @@ import "../../css/pages/index.css";
 
 import Api from "../components/Api";
 import UserInfo from "../components/UserInfo";
+import Section from "../components/Section";
 
 import { configApi, selectorsUserInfo } from "../utils/constants";
+
+// Создания экземпляра карточки и рендер новой карточки
+const renderCard = (cardData) => {
+  //создание и рендер карточки
+}
 
 // Создания экземпляра api
 const api = new Api(configApi);
 // Создания экземпляра пользователя
 const user = new UserInfo(selectorsUserInfo);
+// Создания экземпляра Section для рендера карточек
+const gallery = new Section('.gallery__list', renderCard);
 
 const getData = async () => {
   const serverData = await Promise.all([api.getUser(), api.getCards()]);
@@ -19,6 +27,9 @@ const getData = async () => {
   user._id = userData._id;
   user.setUserInfo(userData);
   user.setUserAvatar(userData);
+
+  //Рендер карточек на странице
+  gallery.rendered(cards);
 };
 
 getData();
